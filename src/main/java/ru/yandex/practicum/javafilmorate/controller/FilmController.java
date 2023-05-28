7ad5c30f30bd4be2ab340b32e09d9a146cef94ee
Дмитрий <dkorokhov@yandex.ru>
2023-05-28 04:02:44 +0300

    1 сдача ТЗ_11

--- src/main/java/ru/yandex/practicum/javafilmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/FilmController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.javafilmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.javafilmorate.model.Film;
 import ru.yandex.practicum.javafilmorate.service.FilmService;
--- src/main/java/ru/yandex/practicum/javafilmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/controller/MpaController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.javafilmorate.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
